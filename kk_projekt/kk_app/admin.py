66513f5c510f3ffe66861643f0abe14fc5ed9a36
skrzypeklaudia <kskrzypek3@st.swps.edu.pl>
2025-01-24 21:11:34 +0100

    04_models

--- kk_projekt/kk_app/admin.py
+++ kk_projekt/kk_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Album, Song, Concert, Collaboration
+
+admin.site.register(Album)
+admin.site.register(Song)
+admin.site.register(Concert)
+admin.site.register(Collaboration)
--- kk_projekt/kk_app/models.py
+++ kk_projekt/kk_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=100, help_text="Tytuł albumu Skolima", blank=False, null= False) # to dodaje tam gdzie sie wpisuje co trzeba wpisac
+    release_date = models.DateField(help_text="Data wydania albumu", editable="False")
+
+    def __str__(self):
+        return f"Album: {self.title} ({self.release_date.year})"
+    
+    
+class Song(models.Model):
+    title = models.CharField(max_length=100, help_text="Tytuł piosenki Skolima", blank=False, null= False)
+    duration = models.DurationField(help_text="Czas trwania piosenki (HH:MM:SS)", blank=False, null=False)
+    album = models.ForeignKey(Album, related_name='songs', on_delete=models.CASCADE, blank= False)
+    release_date = models.DateField(help_text="Data wydania piosenki")
+    
+    def __str__(self):
+        return f"Piosenka: {self.title} (Z albumu: {self.album.title})"
+
+class Concert(models.Model):
+    artist = models.CharField(max_length=100, default="Skolim", help_text="Artysta występujący")  # Domyślnie Skolim
+    location = models.CharField(max_length=200, help_text="Miejsce koncertu", blank=False, null= False)
+    date = models.DateField(help_text="Data koncertu")  # Data koncertu
+
+    def __str__(self):
+        return f"{self.artist} - {self.location} ({self.date})"
+    
+class Collaboration(models.Model):
+    artist = models.CharField(max_length=100, blank=False, null= False)
+    song = models.ForeignKey('Song', related_name='collaborations', on_delete=models.CASCADE, blank=False, null= False)  # Wybór piosenki z wcześniej dodanych
+    def __str__(self):
+        return f"Współpraca z {self.artist} - Piosenka: {self.song.title}"
